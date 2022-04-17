1d4da1a7cea0b6bbc42e8e47e6221fcb86b9dad0
TheRealVincentio <StephStyffe@gmail.com>
2022-04-17 15:44:21 -0700

    added required comments to source code.

--- src/main.js
+++ src/main.js
@@ -13,3 +13,9 @@ let borderUISize = game.config.height / 15;
 let borderPadding = borderUISize / 3;
 
 let explosions = ['sfx_explosion1','sfx_explosion2','sfx_explosion3','sfx_explosion4'];
+
+//Stephanie Styffe Garden Patrol 04/17/22 ~10 hours
+// Implement a simultaneous two-player mode (30)
+// Redesign the game's artwork, UI, and sound to change its theme/aesthetic (to something other than sci-fi) (60)
+// Create 4 new explosion SFX and randomize which one plays on impact (10)
+//total 100, yeah it's not that good but I'm not complaining.
